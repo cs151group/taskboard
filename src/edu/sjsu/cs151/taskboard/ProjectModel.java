8e6690684fcb37e011407db129d33f987609dabd
juan <juan@jrmacias.com>
2018-05-03 21:47:58 -0700

    1. Added functionality and constructor to ProjectModel.java

--- src/edu/sjsu/cs151/taskboard/ProjectModel.java
+++ src/edu/sjsu/cs151/taskboard/ProjectModel.java
@@ -1,6 +1,7 @@
 package edu.sjsu.cs151.taskboard;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 /* Every method of ProjectModel requires a ColumnModel parameter!
  * Is this a cause for concern? 
@@ -10,23 +11,32 @@ public class ProjectModel {
 	private String name = "Project1";
 	private ArrayList<ColumnModel> columns;
 
-	public void moveLeft(ColumnModel col) {
+	public ProjectModel(String name, ArrayList<ColumnModel> columns) {
+		this.name = name;
+		this.columns = columns;
+	}
 
+	public void moveLeft(ColumnModel col) {
+		int currentIndex = columns.indexOf(col);
+		int leftIndex = currentIndex - 1;
+		Collections.swap(columns, currentIndex, leftIndex);
 	}
 
 	public void moveRight(ColumnModel col) {
+		int currentIndex = columns.indexOf(col);
+		int rightIndex = currentIndex + 1;
+		Collections.swap(columns, currentIndex, rightIndex);
+	}
+
+	// TODO: 5/3/18 How are we calling this function? How will we know which col we are moving task from
+	// (delete task from that list)
+	public void changeTaskStatus(ColumnModel col, TaskModel task) {
+		col.addTask(task);
 
 	}
 
-	public void changeTaskStatus(ColumnModel newCol, TaskModel task) {
-
-	}
-	
 	public void addTask(ColumnModel col, TaskModel task) {
-		
+		col.addTask(task);
 	}
-	
-	public void editTask(ColumnModel col , TaskModel task) {
-		
-	}
+
 }
