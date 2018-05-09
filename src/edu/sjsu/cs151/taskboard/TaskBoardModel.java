9ee3a19d81076df8bafe75fccc12c4e6d44b9a50
juan <juan@jrmacias.com>
2018-05-08 23:20:25 -0700

    Oh yeah...getters and setters and stuff.

--- src/edu/sjsu/cs151/taskboard/TaskBoardModel.java
+++ src/edu/sjsu/cs151/taskboard/TaskBoardModel.java
@@ -10,10 +10,12 @@ public class TaskBoardModel {
     private ArrayList<ProjectModel> projects;
     private String fileName;
 
+    public TaskBoardModel() {
+
+    }
 
     public TaskBoardModel(String fileName) {
         // TODO: 5/8/18 Load a TaskBoard from a fileName
-
     }
 
     public TaskBoardModel(String name, ArrayList<ProjectModel> projects, String fileName) {
@@ -21,6 +23,36 @@ public class TaskBoardModel {
         this.projects = projects;
         this.fileName = fileName;
     }
+
+
+    // Begin Getters and Setters
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<ProjectModel> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(ArrayList<ProjectModel> projects) {
+        this.projects = projects;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    // End Getters and Setters
+
 
     public void save() throws FileNotFoundException {
         // TODO: 5/3/18 XML Shit goes here
@@ -50,9 +82,6 @@ public class TaskBoardModel {
         projects.add(p);
     }
 
-    public String getName() {
-        return name;
-    }
 
     // What is this? -J
     public ProjectModel getCurrentProject() {
