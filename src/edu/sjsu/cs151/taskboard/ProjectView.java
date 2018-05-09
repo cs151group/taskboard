0e4ecc8ff2d8290eef41712f30b9f4113029e7fe
juan <juan@jrmacias.com>
2018-05-09 02:54:34 -0700

    Working on NewColumn functionality within ProjectView.java; it's a pain in the `ass`

--- src/edu/sjsu/cs151/taskboard/ProjectView.java
+++ src/edu/sjsu/cs151/taskboard/ProjectView.java
@@ -1,8 +1,11 @@
 package edu.sjsu.cs151.taskboard;
 
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import java.awt.*;
 import java.awt.event.MouseEvent;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 //import javafx.geometry.*;
 import javafx.geometry.Insets;
@@ -11,16 +14,13 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.control.TextField;
 
 public class ProjectView {
 	private Stage primaryStage;
+	private ArrayList<ColumnModel> columnList = new ArrayList<>();
 	
 	public ProjectView(Stage primaryStage) {
 		this.primaryStage = primaryStage;
@@ -63,32 +63,105 @@ public class ProjectView {
         Text colText = new Text (10, 50, "Columns");
         GridPane.setConstraints(colText, 0, 1);
         grid.getChildren().add(colText);
-        
+
+        /*
+		Button minusButton = new Button();
+		minusButton.setText(" - ");
+		GridPane.setConstraints(minusButton, 3, 1);
+		grid.getChildren().add(minusButton);
+        */
+
         Button plusButton = new Button();
         plusButton.setText("+");
-        GridPane.setConstraints(plusButton, 1, 1);
+        GridPane.setConstraints(plusButton, 4, 1);
         grid.getChildren().add(plusButton);
-        
-		int hCounter = 0;
-		int vCounter = 2;
+
+
+		//int ogCol_colIndex = 1;
+		//int ogCol_rowIndex = 1;
+		ArrayList<Integer> i = new ArrayList<>();
+		i.add(1);
+
+		ArrayList<TextField> colFields = new ArrayList<>();
+		ArrayList<Button> minusButtons = new ArrayList<>();
+
+		/*
+		TextField currentColField = new TextField();
+		colFields.add(currentColField);
+		currentColField.setPromptText("Enter column name");
+		GridPane.setConstraints(currentColField, 1, 1);
+		grid.getChildren().add(currentColField);*/
+
+
 		
 		
         plusButton.setOnMousePressed((event) -> {
-        	
-            final TextField colField = new TextField();
-            colField.setPromptText("Enter column name");
-            GridPane.setConstraints(colField, hCounter, vCounter + event.getClickCount());
-            grid.getChildren().add(colField);
-            
-            Button minusButton = new Button();
-            minusButton.setText("-");
-            GridPane.setConstraints(minusButton, hCounter + 1, vCounter + event.getClickCount());
-            grid.getChildren().add(minusButton);
+
+        	/*
+        	final TextField currentColField = new TextField();
+			colFields.add(currentColField);
+			currentColField.setPromptText("Enter column name");
+			GridPane.setConstraints(currentColField, 1, i.size());
+			grid.getChildren().add(currentColField);
+
+			Button minusButton = new Button();
+			minusButton.setText(" - ");
+			GridPane.setConstraints(minusButton, 3, 1);
+			grid.getChildren().add(minusButton);
+
+
+
+			// TODO: 5/9/18 Move this to event listener for "Create" button
+
+			// When Create is clicked, then get the names from the column fields
+			if ((currentColField.getText() != null && !currentColField.getText().isEmpty())) {
+				//columnList.add(new ColumnModel(currentColField.getText(), null));
+				System.out.println(currentColField.getText());
+			}
+
+
+
+			i.add(1);
+			System.out.println("+ BUTTON PRESSED!");
+			System.out.println(colFields.size());
+			for (int j = 0; j < colFields.size(); j++) {
+				System.out.println(colFields.get(j).getText());
+			}
+			*/
+
  
         });
-       
         return grid;
 	}
+
+	private VBox vbox;
+
+	private class NewRow {
+		private HBox hbox;
+		private TextField field;
+		private Button minus;
+
+		public NewRow() {
+			hbox = new HBox();
+			field = new TextField();
+			minus = new Button();
+
+			hbox.getChildren().add(field);
+			hbox.getChildren().add(minus);
+
+			minus.setOnMouseClicked(event -> {
+				vbox.getChildren().remove(this);
+			});
+		}
+
+		public ColumnModel getColumn() {
+			// get field text
+			// return new column
+			return new ColumnModel(field.getText(), null);
+		}
+
+	}
+
 	
 	public AnchorPane addAnchorPane(GridPane grid) 
 	{
