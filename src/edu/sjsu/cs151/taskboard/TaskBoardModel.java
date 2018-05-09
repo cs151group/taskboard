9c58871d8997f64d6455ec88f690e4c4b7687e2e
juan <juan@jrmacias.com>
2018-05-08 23:11:04 -0700

    1. Added load() XMLDecoder to TaskBoardModel.

--- src/edu/sjsu/cs151/taskboard/TaskBoardModel.java
+++ src/edu/sjsu/cs151/taskboard/TaskBoardModel.java
@@ -1,54 +1,62 @@
 package edu.sjsu.cs151.taskboard;
+import javafx.concurrent.Task;
 import java.beans.XMLDecoder;
 import java.beans.XMLEncoder;
 import java.io.*;
 import java.util.ArrayList;
 
 public class TaskBoardModel {
-	private String name = "TaskBoard1";
-	private ArrayList<ProjectModel> projects;
-	private String fileName;
+    private String name = "TaskBoard1";
+    private ArrayList<ProjectModel> projects;
+    private String fileName;
 
 
-	public TaskBoardModel(String fileName) {
-		// TODO: 5/8/18 Load a TaskBoard from a fileName
+    public TaskBoardModel(String fileName) {
+        // TODO: 5/8/18 Load a TaskBoard from a fileName
 
-	}
+    }
 
+    public TaskBoardModel(String name, ArrayList<ProjectModel> projects, String fileName) {
+        this.name = name;
+        this.projects = projects;
+        this.fileName = fileName;
+    }
 
-	public TaskBoardModel(String name, ArrayList<ProjectModel> projects, String fileName) {
-		this.name = name;
-		this.projects = projects;
-		this.fileName = fileName;
-	}
+    public void save() throws FileNotFoundException {
+        // TODO: 5/3/18 XML Shit goes here
+        XMLEncoder e = new XMLEncoder(
+                new BufferedOutputStream(
+                        new FileOutputStream(fileName)
+                ));
+        e.writeObject(this);
+        e.close();
+    }
 
-	public void save() throws FileNotFoundException {
-		// TODO: 5/3/18 XML Shit goes here
-		XMLEncoder e = new XMLEncoder(
-				new BufferedOutputStream(
-						new FileOutputStream("Test.xml")
-				));
-		e.writeObject(this);
-		e.close();
-	}
+    public TaskBoardModel load(String fileName) throws FileNotFoundException {
+        XMLDecoder d = new XMLDecoder(
+                new BufferedInputStream(
+                        new FileInputStream(fileName)));
+        // Will casting work here? - J
+        TaskBoardModel result = (TaskBoardModel) d.readObject();
+        d.close();
+        return result;
+    }
 
+    public void editProject() {
+        // TODO: 5/3/18 do we need to do this here?
+    }
 
-	// TODO: 5/3/18 do we need to do this here?
-	public void editProject() {
-	}
+    public void addProject(ProjectModel p) {
+        projects.add(p);
+    }
 
-	public void addProject(ProjectModel p) {
-		// TODO Auto-generated method stub
-		projects.add(p);
+    public String getName() {
+        return name;
+    }
 
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public ProjectModel getCurrentProject() {
-		return projects.get(0);
-	}
+    // What is this? -J
+    public ProjectModel getCurrentProject() {
+        return projects.get(0);
+    }
 
 }
